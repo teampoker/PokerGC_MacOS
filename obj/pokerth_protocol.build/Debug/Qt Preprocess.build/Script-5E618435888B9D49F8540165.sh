0a4c3f73c11fa633c2bca56f7fc88a02dd9cd902
espisriccardo <espric@inwind.it>
2017-03-07 11:16:19 +0100

    OSX Commit Pre Fianl Release

---
+++ obj/pokerth_protocol.build/Debug/Qt Preprocess.build/Script-5E618435888B9D49F8540165.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/espirmac/Downloads/ODESK_TEMP/PokerTH-1.1-src_Client/ -f pokerth_protocol.xcodeproj/qt_makeqmake.mak
---
+++ obj/pokerth_protocol.build/Debug/pokerth_protocol.build/Script-9C316F444A62BF296E3E1F25.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cp -r $BUILT_PRODUCTS_DIR/$FULL_PRODUCT_NAME /Users/espirmac/Downloads/ODESK_TEMP/PokerTH-1.1-src_Client/lib
